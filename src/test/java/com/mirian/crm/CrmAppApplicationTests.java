86b042c6e168e5d38eda9456206e35b4eaf28d6d
mirian228 <pariksini@mail.ru>
2023-05-31 11:44:58 +0400

    Initialized project

---
+++ src/test/java/com/mirian/crm/CrmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirian.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
