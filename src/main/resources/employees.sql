bade8854b8cc47bfc69f2a22cfa5666f70159159
mirian228 <pariksini@mail.ru>
2023-06-02 16:34:00 +0400

    refactored sql script to create Manager table, add foreign key for employees and populate manager table.

--- src/main/resources/employees.sql
+++ src/main/resources/employees.sql
@@ -16,3 +16,24 @@ INSERT INTO employee VALUES
 		(3, 'Avani', 'Gupta', 'avani@gmail.com'),
         (4, 'Yuri', 'Petrov', 'yuri@gmail.com'),
         (5, 'Juan', 'Vega', 'juan@gmail.com');
+        
+        
+CREATE TABLE manager(
+	id int NOT NULL auto_increment,
+    first_name varchar(45) DEFAULT NULL,
+    last_name varchar(45) DEFAULT NULL,
+    email varchar(45) DEFAULT NULL,
+    primary key(id)
+) ENGINE=InnoDB auto_increment=1 default charset=latin1;
+
+
+ALTER TABLE employee ADD COLUMN manager_id int;
+ALTER TABLE employee ADD FOREIGN KEY (manager_id) REFERENCES manager(id);
+
+INSERT INTO manager VALUES 
+		(1, 'Iuna', 'Vanidze', 'iunona.vanidze@gmail.com'),
+        (2, 'Mirian', 'Surmanidze', 'mirian.surmanidze@gmail.com'),
+        (3, 'Leila', 'Bolkvadze', 'leila.bolkvadze@gmail.com');
+        
+        
+truncate table employee;
