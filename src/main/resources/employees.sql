131ff8cb35c169bb132d42c060cb34ac64c3a692
mirian228 <pariksini@mail.ru>
2023-06-03 15:37:49 +0400

    Added required tables for JDBC Auth, added Datasource to UserDetailsManager so now it uses JDBC for Authentication

--- src/main/resources/employees.sql
+++ src/main/resources/employees.sql
@@ -37,3 +37,35 @@ INSERT INTO manager VALUES
         
         
 truncate table employee;
+
+CREATE TABLE users (
+	username varchar(50) NOT NULL,
+    password varchar(50) NOT NULL,
+    enabled tinyint NOT NULL,
+    
+	PRIMARY KEY(username)
+) Engine=InnoDB default charset=latin1;
+
+INSERT INTO users VALUES
+				('john', '{noop}test123', 1),
+                ('mary', '{noop}test123', 1),
+                ('susan', '{noop}test123', 1);
+
+CREATE Table authorities (
+	username varchar(50) NOT NULL,
+	authority varchar(50) NOT NULL,
+    
+    UNIQUE KEY authorities_idx_1 (username, authority),
+    
+    CONSTRAINT authorities_ibfk_1
+    FOREIGN KEY (username) REFERENCES users(username)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO authorities VALUES 
+('john', 'ROLE_EMPLOYEE'),
+('mary', 'ROLE_EMPLOYEE'),
+('mary', 'ROLE_MANAGER'),
+('susan', 'ROLE_EMPLOYEE'),
+('susan', 'ROLE_MANAGER'),
+('susan', 'ROLE_ADMIN');
+
