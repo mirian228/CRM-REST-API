e1b4764c9364cf05f2aab6e0bd127fe480035c3a
mirian228 <pariksini@mail.ru>
2023-06-03 14:36:33 +0400

    Added SecurityFilterChain configuration to apply requestMatchers for roles on endpoints

--- src/main/java/com/mirian/crm/config/SecurityConfig.java
+++ src/main/java/com/mirian/crm/config/SecurityConfig.java
@@ -2,9 +2,13 @@ package com.mirian.crm.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -32,7 +36,21 @@ public class SecurityConfig {
         return new InMemoryUserDetailsManager(john, mary, susan);
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "api/employees").hasRole("ADMIN"));
+        // use HTTP basic authentication
+        http.httpBasic(Customizer.withDefaults());
+        // disable Cross Site Request Forgery (CSRF) || Not required for basic CRUD REST apis with no HTML forms, if we don't disable can't test with postman
+        http.csrf(csrf -> csrf.disable());
 
-
+        return http.build();
+    }
 
 }
